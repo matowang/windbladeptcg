dddc9a5f08ff5baa88d350cde14c66abe7486ec4
Matthew Wang <wmatthew123@gmail.com>
2020-09-26 20:52:42 +0800

    setup basic grid styles

--- pages/cards.js
+++ pages/cards.js
@@ -33,8 +33,8 @@ const Cards = () => {
                             <div key={card._id} ref={lastCardRef}><Card {...card} /></div> :
                             <Card key={card._id} {...card} />
                     )}
-                    {loading && <h4>loading...</h4>}
                 </div>
+                {loading && <h4>loading...</h4>}
             </main>
         </>
     )
@@ -43,32 +43,7 @@ const Cards = () => {
 export default Cards;
 
 const Card = ({ imageUrl, name }) => (
-    <article>
-        <h2>{name}</h2>
-        <img src={imageUrl} alt={name} />
+    <article class="cards-grid-item">
+        <img className="cards-grid-item__card-img" src={imageUrl} alt={name} />
     </article>
 )
-
-// export async function getServerSideProps() {
-//     const client = createClient();
-//     try {
-//         await client.connect();
-//         const db = client.db('ptcg');
-//         const col = db.collection('cards');
-//         const data = await col.find({ series: "AC1a" }).project({ _id: 0 }).toArray();
-//         console.log(data);
-
-//         return {
-//             props: {
-//                 data: data
-//             }
-//         }
-//     } catch (e) {
-//         console.error(e);
-//         return {
-//             props: {}
-//         }
-//     } finally {
-//         client.close();
-//     }
-// }
