dd59e98ba33d3222c95b6aa91689917f96f6b734
Matthew Wang <wmatthew123@gmail.com>
2020-10-01 20:13:16 +0800

    added expansion dropdown query

--- hooks/useFetchCards.js
+++ hooks/useFetchCards.js
@@ -1,15 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const useFetchCards = (page, query) => {
     const [cards, setCards] = useState([]);
     const [hasNext, setHasNext] = useState(true);
     const [loading, setLoading] = useState(false);
+
+    const fetchController = useRef();
+
     async function fetchCards() {
         console.log("Fetch page", page);
         setLoading(true);
 
-        const res = await fetch(`${process.env.NEXT_PUBLIC_PTCG_API_URL}cards?search=${query}&page=${page}`, {
+        if (fetchController.current) fetchController.current.abort();
+        fetchController.current = new AbortController();
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_PTCG_API_URL}cards/${query.expansion}?search=${query.search}&page=${page}`, {
             method: 'GET',
+            signal: fetchController.current.signal
         });
         const data = await res.json();
 
@@ -18,6 +25,7 @@ const useFetchCards = (page, query) => {
         } else {
             setCards(prevCards => [...prevCards, ...data]);
         }
+
         setLoading(false);
         console.log("datalength", data.length)
         if (data.length < 20)
@@ -28,7 +36,7 @@ const useFetchCards = (page, query) => {
 
     useEffect(() => { fetchCards() }, [page, query]);
 
-    return { cards, hasNext, loading }
+    return { cards, hasNext, loading, setCards, setLoading, fetchController }
 }
 
 export default useFetchCards;
--- pages/cards.js
+++ pages/cards.js
@@ -5,11 +5,16 @@ import { useState, useRef, useCallback, useEffect } from 'react';
 
 import useFetchCards from '../hooks/useFetchCards';
 
+import cardExpansionData from '../data/card-expansion.json';
+
 const Cards = () => {
     const [page, setPage] = useState(1);
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState({
+        search: '',
+        expansion: ''
+    });
 
-    const { cards, hasNext, loading } = useFetchCards(page, query);
+    const { cards, hasNext, loading, setCards, fetchController } = useFetchCards(page, query);
 
     const observer = useRef();
 
@@ -30,9 +35,19 @@ const Cards = () => {
     }, [loading, hasNext]);
 
     const handleSearch = (e) => {
+        e.persist()
         console.log(e.target.value);
+        setCards([]);
         setPage(1);
-        setQuery(e.target.value);
+        setQuery(query => ({ ...query, search: e.target.value }));
+    }
+
+    const handleExpansionDropdown = (e) => {
+        e.persist()
+        console.log(e.target.value);
+        setCards([])
+        setPage(1);
+        setQuery(query => ({ ...query, expansion: e.target.value }));
     }
 
     return (
@@ -40,9 +55,11 @@ const Cards = () => {
             <Header />
             <div id="cards-page">
                 <main>
-                    <div id="search-bar-container">
-                        <SearchBar handleChange={handleSearch} value={query} placeholder="搜尋" />
-                    </div>
+                    <FilterCardsBar
+                        handleSearch={handleSearch}
+                        searchQuery={query.search}
+                        handleExpansionDropdown={handleExpansionDropdown}
+                        expansionDropdownValue={query.expansion} />
 
                     <div className="cards-grid">
                         {cards.map((card, i) =>
@@ -59,6 +76,33 @@ const Cards = () => {
 }
 
 export default Cards;
+
+const FilterCardsBar = ({
+    handleSearch,
+    searchQuery,
+    handleExpansionDropdown,
+    expansionDropdownValue
+}) => (
+        <header className="filter-cards-bar">
+            <form className="filter-cards-bar__form">
+                <div className="search-bar-container">
+                    <SearchBar handleChange={handleSearch} value={searchQuery} placeholder="搜尋" />
+                </div>
+                <ExpansionDropdown handleChange={handleExpansionDropdown} value={expansionDropdownValue} />
+            </form>
+        </header>
+    )
+
+const ExpansionDropdown = ({ handleChange, value }) => (
+    <label>擴充:
+        <select value={value} onChange={handleChange}>
+            {cardExpansionData.map(el =>
+                <option key={el.code} value={el.code}>{el.name}</option>
+            )}
+            <option value="">{"全部"}</option>
+        </select>
+    </label>
+)
 
 const Card = ({ imageUrl, name }) => (
     <article className="cards-grid-item">
